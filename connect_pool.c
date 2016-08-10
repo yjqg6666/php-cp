225db410f1f31f6a300fb47ec87402f442730c2b
郭新华 <393323503@qq.com>
2016-08-10 17:40:24 +0800

    $obj1 = new pdoProxy('mysql:host=192.168.1.19;dbname=mz_db','public_user',"1qa2ws3ed"); $stmt = $obj1->prepare("insert into t1(id,name) values(?,?)"); $stmt->execute(array(2,'1233123123')); $a = $obj1->lastInsertId(); $d = $stmt->rowCount(); var_dump($d); $obj1->release()
    修复这种先stmt 再 pdo 再stmt的用法

--- connect_pool.c
+++ connect_pool.c
@@ -551,18 +551,6 @@ static void pdo_proxy_pdo(zval * args)
             }
 #if PHP_MAJOR_VERSION ==7
             ret_value = (zval *) emalloc(sizeof (zval));
-            if (pdo_stmt)
-            {
-                zval_dtor(pdo_stmt);
-                efree(pdo_stmt);
-                pdo_stmt = NULL;
-            }
-#else
-            if (pdo_stmt)
-            {
-                zval_ptr_dtor(&pdo_stmt);
-                pdo_stmt = NULL;
-            }
 #endif
             if (cp_internal_call_user_function(object, method, &ret_value, args) == FAILURE)
             {
@@ -606,6 +594,11 @@ static void pdo_proxy_pdo(zval * args)
                         zend_get_object_classname(ret_value, (const char **) &name, &name_len TSRMLS_CC);
                         if (strcmp(name, "PDOStatement") == 0)
                         {
+                            if (pdo_stmt)
+                            {
+                                zval_ptr_dtor(&pdo_stmt);
+                                pdo_stmt = NULL;
+                            }
                             pdo_stmt = ret_value;
                             zval send_zval;
                             ZVAL_STRING(&send_zval, "PDOStatement!", 0);
@@ -616,6 +609,12 @@ static void pdo_proxy_pdo(zval * args)
                         zend_string *name = Z_OBJ_HANDLER_P(ret_value, get_class_name)(Z_OBJ_P(ret_value));
                         if (strcmp(name->val, "PDOStatement") == 0)
                         {
+                            if (pdo_stmt)
+                            {
+                                zval_dtor(pdo_stmt);
+                                efree(pdo_stmt);
+                                pdo_stmt = NULL;
+                            }
                             pdo_stmt = ret_value;
                             zval send_zval;
                             CP_ZVAL_STRING(&send_zval, "PDOStatement!", 0);
@@ -677,7 +676,7 @@ static void pdo_proxy_stmt(zval * args)
             cp_zval_ptr_dtor(&str);
             cp_zval_ptr_dtor(&pdo_stmt);
             pdo_stmt = NULL;
-            return;//when the exception,the ret_value dont need dtor
+            return; //when the exception,the ret_value dont need dtor
         }
         if (!ret_value)
         {
